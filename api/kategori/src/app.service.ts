c003abd3607aec4311022049a1730bcbc75ed900
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-03-02 19:49:48 +0700

    8

--- api/kategori/src/app.service.ts
+++ api/kategori/src/app.service.ts
@@ -6,3 +6,4 @@ export class AppService {
     return 'Selamat datang di API Kategori!';
   }
 }
+// logic untuk service utama aplikasi, bisa digunakan untuk menyimpan fungsi-fungsi umum yang dibutuhkan di seluruh aplikasi.
--- api/kategori/src/kategori/kategori.service.ts
+++ api/kategori/src/kategori/kategori.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateKategoriDto } from './dto/create-kategori.dto';
 import { UpdateKategoriDto } from './dto/update-kategori.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class KategoriService {
+  // buat constructor untuk prisma service
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createKategoriDto: CreateKategoriDto) {
     return 'This action adds a new kategori';
   }
